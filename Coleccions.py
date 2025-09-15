0b97cc39bc90e291655d8475d183a83d73ccfb64
oliver <omiguezalonso@gmail.com>
2025-09-15 14:11:15 +0200

    1º Listas 2

--- Coleccions.py
+++ Coleccions.py
@@ -12,5 +12,9 @@ Coleccións:
 l = [23,"Bugario",1,"Hola",[1,2,3,45],-123,-12,2, 3 +4j, True, 1+2,1.2]
 print(type(l))
 print(l)
-print(l[2]) #imprime el 3 valor de la lista
+
+l[1] = "Bugario Brugs" #substituye el 2º valor de la lista por el nuevo
+l[-2] = False
+
+print(l[1]) #imprime el 2 valor de la lista
 print(l[-2]) #imprime el 3 valor empezando por el final
