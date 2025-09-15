84c77396ded1ee543b8f85601319447fc689161b
oliver <omiguezalonso@gmail.com>
2025-09-15 12:58:14 +0200

    1º Numeros Python

---
+++ script.py
@@ -0,0 +1,17 @@
+
+# 1º NUMEROS EN PYTHON
+
+#Enteiros
+numEnteiros = 5
+
+#Coma Flotante
+numDecimal = 5.6
+numDecimal2 = 0.1e-5
+
+#Booleanos
+booleano = True #El True o el False siempre van en mayúscula
+
+#Complexos
+complexo = 2.3 + 9.3j
+
+#Cadeas
