548bda3914e599bed58716d04a1a5b438d65ce0f
oliver <omiguezalonso@gmail.com>
2025-09-15 14:19:18 +0200

    1º Listas 3

--- Coleccions.py
+++ Coleccions.py
@@ -18,3 +18,13 @@ l[-2] = False
 
 print(l[1]) #imprime el 2 valor de la lista
 print(l[-2]) #imprime el 3 valor empezando por el final
+
+print(l[4][2])#recoge el 3 valor de la lista que esta dentro de l
+
+#Slicing
+print(l[2 : 5]) #coge los valores de la lista del 2 al 5 excluyendo al 5 -> [1, 'Hola', [1, 2, 3, 45]]
+print(l[1][2:6]) #Tambien funciona con Strings
+print(l[1][2:-2])
+print(l[:1])#Coge solo el primero
+print(l[1:])#Todos menos el primero
+print(l[1:6:2]) #Coge valores del 1 al 5 de dos en dos
