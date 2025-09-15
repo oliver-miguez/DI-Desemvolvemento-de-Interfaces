ffeb700b545e503cebf717b49da484bb7e664407
oliver <omiguezalonso@gmail.com>
2025-09-15 14:07:35 +0200

    1º Listas

--- Coleccions.py
+++ Coleccions.py
@@ -6,3 +6,11 @@ Coleccións:
 3ºDiccionarios
 
 """
+
+#Listas
+
+l = [23,"Bugario",1,"Hola",[1,2,3,45],-123,-12,2, 3 +4j, True, 1+2,1.2]
+print(type(l))
+print(l)
+print(l[2]) #imprime el 3 valor de la lista
+print(l[-2]) #imprime el 3 valor empezando por el final
