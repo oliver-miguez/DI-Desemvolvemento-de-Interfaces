9de3b2b5827d2b384a412c8ddaa22009b612814b
oliver <omiguezalonso@gmail.com>
2025-09-16 14:00:11 +0200

    1ºCondicionais 2

--- Teoria Básica/Condicionais.py
+++ Teoria Básica/Condicionais.py
@@ -8,5 +8,10 @@ if n1 > 5:
     print("hola")
 else:
     print("Adios")
+    if n1 < 2:
+        print("Este numero es muy pequeño")
+    else:
+        print("Es pequeño pero no tanto")
+
 
 print("if finalizado ya estamos fuera del if")
