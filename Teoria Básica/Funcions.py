4b78b10f8414a2523bb0710430cb345592ccde82
oliver <omiguezalonso@danielcastelao.org>
2025-09-18 08:57:25 +0200

    2ºFunciones

---
+++ Teoria Básica/Funcions.py
@@ -0,0 +1,9 @@
+#Funcións
+
+def nome_da_funcion(parametro1,parametro2):
+    #Instruccions
+    print("parametro1:",parametro1)
+    print("parametro2:",parametro2)
+
+nome_da_funcion(4,2)
+nome_da_funcion(parametro2=4.5,parametro1="Esto cambia el orden de aparción del parámetro")
