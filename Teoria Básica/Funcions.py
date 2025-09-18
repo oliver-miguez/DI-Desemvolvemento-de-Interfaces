4bcb0e225c8214fbf0729b93f49e4e96a3434d4e
oliver <omiguezalonso@danielcastelao.org>
2025-09-18 09:32:18 +0200

    2ºFunciones 2

--- Teoria Básica/Funcions.py
+++ Teoria Básica/Funcions.py
@@ -8,8 +8,20 @@ def nome_da_funcion(parametro1,parametro2):
 nome_da_funcion(4,2)
 nome_da_funcion(parametro2=4.5,parametro1="Esto cambia el orden de aparción del parámetro")
 
-def repetir_mensaxe(mensaxe, veces = 1):
+#para indicar parametros que poden ir de poucos a infinitos usamos o "*" , en estos casos el tipo de valor que representa son tuplas
+def repetir_mensaxe(mensaxe, veces = 1, *maisMensaxes):
+    print(type(maisMensaxes))
+    print("temos un total de",str(len(maisMensaxes)),"parametros extra")
     print(mensaxe * veces)
+    for outraMensaxe in maisMensaxes:
+        print(outraMensaxe * veces)
 
 #Si a la hora de llamar a la función no especificamos el total de voces por defecto pondra 1 tal y como la creamos en la función ( voces = 1 )
-repetir_mensaxe("Ola ",5)
+repetir_mensaxe("Ola ",6,"adeus ","benvido ","python ","java ","godot",3.5)
+
+def persoa (nome,dni,**maisDatos):
+    print("O nome é:",nome)
+    print("O DNI é:",dni)
+    for dato in maisDatos.keys():
+        print("O dato ",dato,"é ",maisDatos[dato])
+persoa("oliver","7862",fecha = 2, nCasa = 17,apelido = "miguez")
