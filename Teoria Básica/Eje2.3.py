2a6ddfc686512d6eacd745278ea6c33da86805d0
oliver <omiguezalonso@danielcastelao.org>
2025-09-22 12:52:59 +0200

    Arreglos

--- Teoria Básica/Eje2.3.py
+++ Teoria Básica/Eje2.3.py
@@ -2,13 +2,8 @@
 Exercicio 2.3. Escribir un programa que converta un valor dado en grados Fahrenheit a grados Celsius. Recordade que a fórmula para a conversión é: F = 9/5 * C + 32.
 """
 
-
-def conversion():
-    fare = 0
-    while fare <= 120:
-        grados = (fare - 32) * 5 / 9
-        fare += 10
-        print(grados)
-conversion()
-
-
+grados = 28
+def conversion(grado):
+    f = (grado - 32) * (5/9)
+    print(f)
+conversion(grados)
--- Teoria Básica/Eje2.4.py
+++ Teoria Básica/Eje2.4.py
@@ -2,9 +2,11 @@
  Utilice o programa anterior para xerar unha táboa de conversión de temperaturas, dende 0º F ata 120º F, de 10 en 10.
 """
 
-def taboa_conversion():
-    f = 0
-    grado = 0
-    while (f <= 120):
-       grado = (f)
+def conversion():
+    fare = 0
+    while fare <= 120:
+        grados = (fare - 32) * 5 / 9
+        fare += 10
+        print(grados)
+conversion()
 
