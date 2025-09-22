d65ade1e49bcd4b88a219da5b82fd7b5969d9fde
oliver <omiguezalonso@danielcastelao.org>
2025-09-22 12:52:07 +0200

    Eje 2.4

---
+++ Teoria Básica/Eje2.4.py
@@ -0,0 +1,10 @@
+"""
+ Utilice o programa anterior para xerar unha táboa de conversión de temperaturas, dende 0º F ata 120º F, de 10 en 10.
+"""
+
+def taboa_conversion():
+    f = 0
+    grado = 0
+    while (f <= 120):
+       grado = (f)
+
