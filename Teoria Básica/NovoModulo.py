a814cdb8dbc9792a5c805745f4a96de9aeff3e4d
oliver <omiguezalonso@danielcastelao.org>
2025-10-06 12:51:02 +0200

    Funcionamiento del import y del empaquetado

--- Teoria Básica/NovoModulo.py
+++ Teoria Básica/NovoModulo.py
@@ -1,5 +1,9 @@
 import Empaquetado # Llama a las funcionalidades del módulo empaquetado
 
+from math import  pi # Importa en el script el "pi" de la libreria de math
+#from math import  * # Importa lo relacionado con la libreria math (NO RECOMENDABLE, CONSUME MUCHA MEMÓRIA)
+
+# Al importar directamente importamos el código dentro del módulo por eso siempre aparece por consola "Un módulo"
 # Llama a los métodos del módulo Empaquetado
 Empaquetado.unha_funcion()
 Empaquetado.UnhaClase()
