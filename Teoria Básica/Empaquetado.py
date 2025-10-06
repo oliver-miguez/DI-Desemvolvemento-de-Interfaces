f3d43b4ef0e18dc97f8649c4c41c4cd1fd456f6d
oliver <omiguezalonso@danielcastelao.org>
2025-10-06 13:00:46 +0200

    Comentarios y varias formas de importar metodos del módulo

--- Teoria Básica/Empaquetado.py
+++ Teoria Básica/Empaquetado.py
@@ -5,4 +5,7 @@ class UnhaClase:
     def __init__(self):
         print("Unha clase")
 
-print("Un módulo") # Unidad más pequeña
+print("Un módulo, esto se muestra siempre") # Unidad más pequeña, siempre que se importe se llama siempre
+
+if __name__ == "__main__": # El código que se escriba dentro de aquí no será mostrado en el resto de módulos que importen esto, ya que esto solo se ejecuta en la main de "Empaquetado"
+    print("Esto no se muestras cuando se importa")
--- Teoria Básica/NovoModulo.py
+++ Teoria Básica/NovoModulo.py
@@ -1,9 +1,16 @@
 import Empaquetado # Llama a las funcionalidades del módulo empaquetado
 
-from math import  pi # Importa en el script el "pi" de la libreria de math
+#from math import  pi # Importa en el script el "pi" de la libreria de math
 #from math import  * # Importa lo relacionado con la libreria math (NO RECOMENDABLE, CONSUME MUCHA MEMÓRIA)
+
+
 
 # Al importar directamente importamos el código dentro del módulo por eso siempre aparece por consola "Un módulo"
 # Llama a los métodos del módulo Empaquetado
-Empaquetado.unha_funcion()
+# 1º Forma de llamar métodos del módulo importado Empaquetado
+#Empaquetado.unha_funcion()
 Empaquetado.UnhaClase()
+
+# 2º Forma de llamar métodos del módulo importado Empaquetado
+from Empaquetado import unha_funcion # Permite utilizar la función "unha_funcion()" sin llamar directamente a la importación para usarla
+unha_funcion()
