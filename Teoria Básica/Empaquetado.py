1af1a37a05d653cd75f77d77f7c3cc29fd744f95
oliver <omiguezalonso@danielcastelao.org>
2025-10-06 12:45:41 +0200

    Empaquetado y refacor de carpetas

---
+++ Teoria Básica/Empaquetado.py
@@ -0,0 +1,8 @@
+def unha_funcion ():
+    print("Unha función")
+
+class UnhaClase:
+    def __init__(self):
+        print("Unha clase")
+
+print("Un módulo") # Unidad más pequeña
---
+++ Teoria Básica/NovoModulo.py
@@ -0,0 +1,5 @@
+import Empaquetado # Llama a las funcionalidades del módulo empaquetado
+
+# Llama a los métodos del módulo Empaquetado
+Empaquetado.unha_funcion()
+Empaquetado.UnhaClase()
